10d65dc366399a60eddb33b3ca39ba835d79dd4d
Daniel Belarmino <belarba@gmail.com>
2021-10-02 00:42:40 +0100

    controller tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -12,7 +12,42 @@ RSpec.describe ContactsController, type: :controller do
       request.accept = "application/json"
       get :index
 
-      expect(assigns(:contacts)).to eq([my_contacts])
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with good email' do
+      it 'should return successful response' do
+        request.accept = "application/json"
+        post :create, contact: {first_name: 'Bruce', last_name: 'Dickinson', email: 'iron@maiden.com', phone: '666'}
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with used email' do
+      before do
+        Contact.create(first_name: 'Bruce', last_name: 'Dickinson', email: 'iron@maiden.com', phone: '666')
+      end
+      it 'should return successful response' do
+        request.accept = "application/json"
+        post :create, contact: {first_name: 'Steve', last_name: 'Harris', email: 'iron@maiden.com', phone: '666'}
+
+        expect(response.response_code).to be(422)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with good data' do
+      before do
+        @contact = Contact.create(first_name: 'Bruce', last_name: 'Dickinson', email: 'iron@maiden.com', phone: '666')
+      end
+      it 'updates the wallet and redirects' do
+        patch :update, id: @contact.id, contact: {first_name: 'Daniel', last_name: @contact.last_name, email: @contact.email, phone: @contact.phone}
+        expect(response).to redirect_to(@contact)
+      end
     end
   end
 
